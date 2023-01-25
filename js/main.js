2e186a5c2525caab1eca70802223037313baef3e
steph14mat <sgobbe@hotmail.com>
2023-01-25 10:11:23 +0100

    HTML, CSS and Header with JS ok

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 3,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+    speed: 1200,
+  });
